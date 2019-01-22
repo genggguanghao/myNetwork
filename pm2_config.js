34e03b81411b8514ad881b495332ba301bc1c4e1
genggguanghao <15103785516@163.com>
2019-01-22 21:59:18 +0800

    pm2自动化

--- pm2_config.js
+++ pm2_config.js
@@ -4,11 +4,8 @@ module.exports = {
           name: "FQY",
           script: "./dist/app.js",
           watch: true,
-          env_development: {
+          env: {
             "NODE_ENV": "development"
-          },
-          env_uat: {
-            "NODE_ENV": "uat"
           },
           env_production: {
             "NODE_ENV": "production",
